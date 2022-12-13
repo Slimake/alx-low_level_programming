b0d88dfe259ac57ac88a810230e010a27614c5b8
Sunday <marcusitovie@gmail.com>
2022-12-13 03:40:55 -0800

    9 print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,12 @@ int main(void)
 
 	for (ch = '0'; ch <= '9'; ch++)
 	{
-		if (ch != '9')
+		putchar(ch);
+		if (ch < '9')
 		{
-			putchar(ch);
 			putchar(',');
+			putchar(' ');
 		}
-
-		else 
-			putchar(ch);
 	}
 	putchar('\n');
 	return (0);
