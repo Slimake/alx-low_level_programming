e31e3d019208ecf3da309946439a508755621e3f
Sunday <marcusitovie@gmail.com>
2022-12-17 08:02:14 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign to the stdout
+ * @n: integer to be checked
+ *
+ * Return: 1 (if n is greater than zero)
+ * 0 (if n is equal to zero)
+ * -1 (if n is less than zero)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
