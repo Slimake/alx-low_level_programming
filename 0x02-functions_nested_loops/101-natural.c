2f8a621510467323211ca174622d8dcb3ffbce00
Sunday <marcusitovie@gmail.com>
2022-12-19 09:14:07 -0800

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int n = 1024;
+	int result = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if ((i % 3) == 0)
+			result += i;
+		else if ((i % 5) == 0)
+			result += i;
+	}
+	printf("%d \n", result);
+
+	return (0);
+}
