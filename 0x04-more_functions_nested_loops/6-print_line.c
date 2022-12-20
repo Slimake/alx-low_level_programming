c5b4055e9db38d333e77a075f473ce849cfeef67
Sunday <marcusitovie@gmail.com>
2022-12-20 14:11:14 -0800

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ * @n: number of lines to draw
+ *
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
