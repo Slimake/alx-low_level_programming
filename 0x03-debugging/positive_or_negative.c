23d1e5db5b9106c49482f40d86bcc87caed0ed2b
Sunday <marcusitovie@gmail.com>
2022-12-19 19:15:07 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * positive_or_negative - check signed random number
+ *
+ * @i: number to check
+ */
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
