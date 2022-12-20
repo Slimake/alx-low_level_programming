b062aa7ad9cb8ba2c0a3f615c67110dd4bb7db01
Sunday <marcusitovie@gmail.com>
2022-12-19 17:52:08 -0800

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,40 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_to_98 - print any nber to 98 to the stdout
  * @n: integer nber
 */
+
 void print_to_98(int n)
 {
 	int stopNum = 98;
 
-	if (n >= 0 && n <= stopNum)
+	if (n >= stopNum)
 	{
-		while (n != (stopNum + 1))
-		{
-			printf("%d", n);
-			if (n < stopNum)
-				printf(", ");
-			n++;
-		}
-	} else if (n > stopNum)
-	{
-		while (n != (stopNum - 1))
-		{
-			printf("%d", n);
-			if (n > stopNum)
-				printf(", ");
-			n--;
-		}
+		while (n > stopNum)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	} else
 	{
-		while (n != (stopNum + 1))
-		{
-			printf("%d", n);
-			if (n < stopNum)
-				printf(", ");
-			n++;
-		}
+		while (n < stopNum)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 	printf("\n");
 }
