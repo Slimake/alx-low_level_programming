fb2aba4d0bf282f00214308305bad4a59782b160
Sunday <marcusitovie@gmail.com>
2022-12-21 01:08:28 -0800

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle shape on the screen
+ * @size: size of triangle shape
+ *
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j <= size; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
