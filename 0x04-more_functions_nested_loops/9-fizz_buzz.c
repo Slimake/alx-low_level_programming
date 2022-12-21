8d453315e5c25dd80f884c3821e5aac4a4becc3a
Sunday <marcusitovie@gmail.com>
2022-12-20 16:23:44 -0800

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int num = 100;
+
+	for (i = 1; i <= num; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
