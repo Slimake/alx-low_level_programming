5aadba55bda307bb5e7e3ad285ed050633c60799
Sunday Ajakaiye <marcusitovie@gmail.com>
2022-12-30 17:19:45 +0000

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *@s: a pointer
+ *
+ * Return: An integer value
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while(*(s + count) != '\0')
+	{
+		count++;
+		printf("%d\n", count);
+	}
+
+	return count;
+}
