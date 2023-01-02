682bbf0c43d3460739dc8316aaac1cf6a832a498
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-01-02 12:32:41 +0000

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * @dest: parameter pointer
  * @src: parameter
  *
  * Return: dest
  */
 
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 	int count;
