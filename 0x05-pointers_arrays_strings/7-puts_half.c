106ac5f2bafa0f955659161a3d91e9bc589e18e7
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-01-02 07:43:08 +0000

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: parameter
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int count;
+
+	i = 0;
+	count = 0;
+
+	while (*(str + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	n = (count / 2);
+
+	for (i = n; i < count; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
