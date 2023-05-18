b7536524802c010d00c80020658094338c104eda
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-18 22:57:10 +0000

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+  * main - prints a defined string to the screen
+  *
+  *	return 0
+  */
+
+#include <stdio.h>
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
