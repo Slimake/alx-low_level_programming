0cdae96ca6792a92f07a01745ea6c2477a3b3887
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-18 23:28:27 +0000

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - display text on the screen
+ *
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
