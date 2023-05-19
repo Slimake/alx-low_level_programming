f49e87885d5eb0fe017620bec30f8421528333e4
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-19 23:45:31 +0000

    Add readme file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+/**
+ * main - a program to assign random number
+ *
+ * Return: random number
+ */
+
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	if (n == 0)
+		printf("%d is zero", n);
+	if (n < 0)
+		printf("%d is negative", n);
+
+	return (0);
+}
