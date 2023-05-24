5d29e08a50130c9071fd47b1e92d80ca0df37dec
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-24 16:22:06 +0000

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 98 Fibonacci numbers
+ * starting with 1 and 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long prev = 1;
+	long cur = 2;
+	long result = 0;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i == 1)
+			printf("%lu, ", prev);
+		else if (i == 2)
+			printf("%lu", cur);
+		else
+		{
+			printf(", %lu", result);
+		}
+
+		result = (prev + cur);
+		prev = cur;
+		cur = result;
+
+	}
+
+	printf("\n");
+	return (0);
+}
