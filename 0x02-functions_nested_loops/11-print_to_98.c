844b417ffa6a341d21e38f9bb5a79d55262a8735
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-24 04:51:09 +0000

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,8 +4,7 @@
 /**
  * print_to_98 - print all natural numbers
  * from n to 98
- * @num1: parameter1
- * @num2: parameter2
+ * @n: parameter2
  *
  * Return: Always 0.
  */
@@ -23,6 +22,8 @@ void print_to_98(int n)
 		n--;
 	}
 	if (n == 98)
-			printf("%i", n);
+	{
+		printf("%i", n);
+	}
 	printf("\n");
 }
