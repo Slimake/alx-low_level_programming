ca4b60abc9aee3e124a898de6191d9d49056e50d
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-24 21:21:31 +0000

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,9 @@ int print_last_digit(int num)
 	long n = num;
 
 	if (n < 0)
-	{
-		n = ((n) * (-1));
-		_putchar('0' + (n % 10));
-	}
-	else if (n >= 0)
-		_putchar('0' + (n % 10));
+		n *= -1;
+
+	_putchar('0' + (n % 10));
 
 	return (n % 10);
 }
