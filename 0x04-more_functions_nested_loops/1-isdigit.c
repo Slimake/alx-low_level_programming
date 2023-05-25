6dd6fbd1730d62de178055125142143e48336aca
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-25 17:02:47 +0000

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks for a digit(0-9)
+ * @c: parameter
+ * Return: found(1), not found(0)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);  
+	else
+		return (0);
+}
