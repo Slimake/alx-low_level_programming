e827c402584581ff5744dcac6d8d3287eff82ba0
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-25 18:39:21 +0000

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 	int i = 0;
 	int j;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		j = 0;
 
-		while (j <= 14)
+		while (j < 15)
 		{
-			if (j >= 10)
+			if (j > 9)
 				_putchar('0' + (j / 10));
 
 			_putchar('0' + (j % 10));
