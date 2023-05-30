fe19130a3703dedb6c7cba6c22b4b583f424be81
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-30 20:54:30 +0000

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to char
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int len = 0;
+	char temp;
+	char *start = s;
+	char *end = s;
+
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (i = 0; i < (len - 1); i++)
+	{
+		end++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+
+	_putchar('\n');
+}
