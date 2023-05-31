7b0b21cec722bb648545555796ce09e89712b1c5
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-31 19:52:51 +0000

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - copies a string
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: parameter variable
+ * Return: pointer to char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
