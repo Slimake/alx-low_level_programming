77e64644386247bfcfa108d76b0a66a971fa8ef6
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-31 22:05:04 +0000

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to char
+ * @s2: pointer to char
+ * Return: an integer value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	int result = 0;
+
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			result = 0;
+		}
+		else if (s1[i] < s2[i])
+		{
+			result  = -15;
+			break;
+		}
+		else if (s1[i] > s2[i])
+		{
+			result = 15;
+			break;
+		}
+	}
+	return (result);
+}
+
+
