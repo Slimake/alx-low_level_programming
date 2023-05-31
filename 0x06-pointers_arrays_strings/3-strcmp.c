cbef061d8102b4593b1522e6bf490210f852f871
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-05-31 23:27:45 +0000

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int result;
+	int len = 0;
 
 	for (i = 0; s2[i] != '\0'; i++)
-	{
+		len++;
+
+	s2[i] = '\0';
+
+	for (i = 0; i < len + 1; i++)
+	{	
 		if (s1[i] == s2[i])
 		{
 			result = 0;
