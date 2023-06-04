6ea908b4594f91c5dd6c34b9e5a34235d07684cd
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-04 14:59:20 +0000

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,34 @@
+#include <stdio.h>
 /**
  * rot13 - encodes a string using rot13
  * @str: pointer to char
  * Return: pointer to str which has a type char
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i;
+	int j;
 
-	for (i = 0; str[i] != '\0'; i++)
+	char alpha[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	char alpha13[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] + 13 <= 'z')
-			str[i] = str[i] + 13;
-		else if (str[i] >= 'a' && str[i] + 13 > 'z')
-			str[i] = str[i] - 13;
-		else if (str[i] >= 'A' && str[i] + 13 <= 'Z')
-			str[i] = str[i] + 13;
-		else if (str[i] >= 'A' && str[i] + 13 > 'Z')
-			str[i] = str[i] - 13;
+	
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = alpha13[j];
+				break;
+			}
+		}	
+
 	}
 
-	return (str);
+	return (s);
+
 }
