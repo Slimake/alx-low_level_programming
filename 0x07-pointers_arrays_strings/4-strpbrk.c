a116bd436ec506f4a74c6c54071b09ad215de25f
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-05 22:58:34 +0000

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: the memory address to be searched
@@ -10,28 +12,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
-	int str_len = 0;
-	int count = 0;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		str_len++;
-		i++;
-	}
-
-	for (i = 0; i <= str_len && count < 1; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				count++;
-				i--;
-				break;
+				return (s + i);
 			}
 		}
 	}
 
-	return (s + i);
+	return (NULL);
 }
