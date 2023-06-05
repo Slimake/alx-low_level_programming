7113c5e89fdd97af8836aa2b5d0fa4ff18de15aa
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-05 22:27:21 +0000

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the memory address to be searched
+ * @accept: the memory address to be use for the search
+ * Return: a pointer to the byte in s that matches one of
+ * the byte in accept or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (s + i);
+}
