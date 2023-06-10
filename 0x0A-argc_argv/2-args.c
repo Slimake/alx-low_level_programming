5a391cd4497df5c812897a624a9a525b99d29852
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-10 01:11:21 +0000

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments received
+ * @argc: number of arguments passed
+ * @argv: arguments passed
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
