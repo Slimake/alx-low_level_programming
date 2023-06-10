ad9e78740e2cafbba7caeefaf6d978a5b786cbca
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-10 00:56:51 +0000

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed
+ * @argv: arguments passed
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return(0);
+}
