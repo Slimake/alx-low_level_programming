ed04fff4b6ef6c0449ad99ace380275236415478
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-16 23:29:38 +0000

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	/* allocate memory */
 	str = malloc(sizeof(char) * get_s1Len(s1) * get_s2Len(s2, n));
 
