e9d47a444e35aec543dbe6235765ea0bdb35573d
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-17 02:55:25 +0000

    3-array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,18 +10,18 @@
 int *array_range(int min, int max)
 {
 	int i;
-	int *ptr;
+	void *ptr;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(max) * max + 1);
+	ptr = malloc(sizeof(int) * max + 1);
 
 	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i <= max; i++)
-		*(ptr + i) = i;
+		*((int *)ptr + i) = i;
 
 	return (ptr);
 }
