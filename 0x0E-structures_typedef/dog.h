5941a3501edbcd308db35e3ba41cb1dfaaf4eec9
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-06-21 11:00:51 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct with tag name dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
