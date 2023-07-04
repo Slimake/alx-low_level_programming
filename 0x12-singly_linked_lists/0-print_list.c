54c61247ee0f2f9430d7c36c7f7d8cd6ded37122
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-07-04 01:10:23 +0000

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to struct list_s
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	if (h == NULL)
+	{
+		printf("List is empty");
+		exit(EXIT_FAILURE);
+	}
+
+	while (h != NULL)
+	{
+		count++;
+
+		if (h->str == NULL)
+			printf("[0] %p\n", h->str);
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (count);
+}
