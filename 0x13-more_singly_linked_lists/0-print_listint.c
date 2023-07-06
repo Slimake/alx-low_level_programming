efe7045bcfb8f4a58ccb4bc782e4c08d2fab54d7
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-07-06 00:25:14 +0000

    Update 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h != NULL)
-		{
-			count++;
-			printf("%i\n", h->n);
-			h = h->next;
-		}
+		count++;
+		printf("%i\n", h->n);
+		h = h->next;
 	}
 
 	return (count);
