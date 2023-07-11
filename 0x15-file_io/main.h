bd6ef0c2c9bb730579bd222b4194db5fdc60836a
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-07-11 01:52:48 +0000

    Update main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,9 +9,6 @@
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERROR_FILENO 2
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
