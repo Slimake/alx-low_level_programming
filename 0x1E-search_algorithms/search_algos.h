b0c99a73f7a4140e3b748d4155ecede872944f7a
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-12-15 21:28:37 +0000

    Add binary search prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/* Function prototype */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void displayArray(int *array, int startIndex, int endIndex);
 
 #endif /* SEARCH_ALGOS_H */
