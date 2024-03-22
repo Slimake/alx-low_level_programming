4b122a6afa4bbf934ce776a71e72f31b0267024e
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-22 14:31:34 +0000

    Refactor code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,41 +11,39 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
+	unsigned int len = 0;
 	dlistint_t *current = *h;
+	dlistint_t *head_len = *h;
 	dlistint_t *newNode = NULL;
 
-	/* Check if h is NULL */
 	if (h == NULL)
 		return (NULL);
 
-	/* Check if *h is NULL */
 	if (*h == NULL)
 		return (NULL);
 
-	/* Allocate memory on the heap to newNode */
 	newNode = malloc(sizeof(dlistint_t));
-	/* Return NULL is memory allocation was not successful */
 	if (newNode == NULL)
 		return (NULL);
-
-	/* set newNode data to n */
 	newNode->n = n;
 
-	/* Cycle through until a node before idx position */
+	while (head_len != NULL)
+	{
+		len += 1;
+		head_len = head_len->next;
+	}
+
+	if (idx < 1 || idx > len - 1)
+		return (NULL);
+
 	while (i < (idx - 1))
 	{
 		current = current->next;
 		i++;
 	}
 
-	if (idx < 1 || idx > (i + 1))
-		return (NULL);
-
-	/* set newNode->next to current->next and newNode->prev to current */
 	newNode->next = current->next;
 	newNode->prev = current;
-
-	/* set current->next to newNode and newNode->next->prev to newNode */
 	current->next = newNode;
 	newNode->next->prev = newNode;
 
