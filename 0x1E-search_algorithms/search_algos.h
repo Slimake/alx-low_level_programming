7be1d782201c5499593df14fe9f9c5ada8985bed
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-17 14:39:32 +0100

    Add binary_search prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-/* Standard libary functions used */
-#include <stdio.h>
-
-
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
